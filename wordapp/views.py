d2046025e98393a3257fba45ce191fa229e7ddff
sehuikwon <sehui4011@hanmail.net>
2019-05-02 17:00:30 +0900

    first commit

---
+++ wordapp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def count(request):
+    full_text = request.GET['fulltext']
+    word_list = full_text.split()
+    word_dictionary = {}
+    for word in word_list:
+        #increase
+        if word in word_dictionary: 
+            word_dictionary[word] +=1
+        #넣어주기
+        else:
+            word_dictionary[word] = 1
+
+    return render(request, 'count.html', {"fulltext":full_text, 'total':len(word_list), 'dictionary':word_dictionary.items()})
